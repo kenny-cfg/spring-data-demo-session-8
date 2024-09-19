f5a6e50c42c6cda846eabd68f072bf44de50944a
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-19 20:09:03 +0100

    Configure JDBC

--- src/main/java/com/codefirstgirls/spring/data/data_demo/DataDemoApplication.java
+++ src/main/java/com/codefirstgirls/spring/data/data_demo/DataDemoApplication.java
@@ -12,19 +12,6 @@ public class DataDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataDemoApplication.class, args);
-
-        val book = Book.builder()
-                .title("The Title")
-                .year(1969)
-                .author("May Dupp")
-                .price(BigDecimal.TEN)
-                .build();
-        val amendedBook = book.toBuilder()
-                .author("Mary Ann")
-                .build();
-
-        System.out.println("First book author: " + book.getAuthor());
-        System.out.println("Second book author: " + amendedBook.getAuthor());
     }
 
 }
