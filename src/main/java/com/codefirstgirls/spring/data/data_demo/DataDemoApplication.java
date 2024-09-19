e526c5e6cf859df227a0a9ce0e15f40d3a5141e5
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-19 19:11:29 +0100

    @Data example

--- src/main/java/com/codefirstgirls/spring/data/data_demo/DataDemoApplication.java
+++ src/main/java/com/codefirstgirls/spring/data/data_demo/DataDemoApplication.java
@@ -1,6 +1,7 @@
 package com.codefirstgirls.spring.data.data_demo;
 
 import com.codefirstgirls.spring.data.data_demo.model.Book;
+import lombok.val;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,22 +13,14 @@ public class DataDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DataDemoApplication.class, args);
 
-		/*
-		var book = new Book(
-		"The Title",
-		1969,
-		"May Dupp",
-		BigDecimal.TEN
-		)
-		 */
-		var book = Book.builder()
+		val book = Book.builder()
 				.title("The Title")
 				.year(1969)
 				.author("May Dupp")
 				.price(BigDecimal.TEN)
 				.build();
 
-		System.out.println(book);
+		System.out.println(book.getTitle());
 	}
 
 }
--- src/main/java/com/codefirstgirls/spring/data/data_demo/model/Book.java
+++ src/main/java/com/codefirstgirls/spring/data/data_demo/model/Book.java
@@ -1,13 +1,15 @@
 package com.codefirstgirls.spring.data.data_demo.model;
 
 import lombok.Builder;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Builder
+@Data
 public class Book {
-    private String title;
-    private int year;
-    private String author;
-    private BigDecimal price;
+    private final String title;
+    private final int year;
+    private final String author;
+    private final BigDecimal price;
 }
